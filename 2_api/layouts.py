df26162f27b1bd0df44796156c16d29aa82a4bf1
Paul Fannon <paul.fannon@wwt.com>
2024-08-12 16:58:03 -0500

    Add column, expander, container and empty elements

--- 2_api/layouts.py
+++ 2_api/layouts.py
@@ -22,3 +22,42 @@ side_bar.write(f'You selected the column name = {select_column}')
 
 # Display the data frame
 st.dataframe(df[[select_column]])
+
+# Layout columns
+st.header('Columns: st.columns')
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.subheader('Column 1')
+    st.image('./media/image.jpg')
+
+with col2:
+    st.subheader('Column 2')
+    st.dataframe(df)
+
+with col3:
+    st.subheader('Column 3')
+    st.dataframe(df[[select_column]])
+
+# expander
+st.subheader('Expander: st.expander')
+with st.expander('My expander'):
+    st.write("""
+    I'm a little teapot, short and stout
+    """)
+    st.code('print("Here is my handle, here is my sprout")', language='python')
+
+# container
+st.subheader('Container st.container')
+with st.container():
+    st.write('when I get all steamed up, hear me shout')
+
+# Empty
+st.subheader('Empty: st.empty')
+placeholder = st.empty()
+
+for i in range(10, 0, -1):
+    placeholder.write(f'TIP ME OVER AND POUR ME OUT IN THE NEXT {i} SECONDS')
+    time.sleep(1)
+
+placeholder.empty()
