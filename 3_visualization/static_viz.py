1ed335f74c86633019eaee71e324d267eff9e4f9
Paul Fannon <paul.fannon@wwt.com>
2024-08-13 14:40:35 -0500

    Implement bar and pie charts

--- 3_visualization/static_viz.py
+++ 3_visualization/static_viz.py
@@ -16,3 +16,27 @@ st.dataframe(df.head())
 # 2. Find distribution of male and female spent (boxplot or kdeplot)
 # 3. Find distribution of average total_bill across each day by male and female
 # 4. Find the relation between total_bill and tip on time (scatter plot)
+
+st.markdown('---')
+with st.container():
+    st.write('1. Find number of male and female distribution (pie and bar)')
+
+    value_counts = df['sex'].value_counts()
+    col1, col2 = st.columns(2)
+
+    with col1:
+        # draw pie chart
+        st.subheader('Pie chart')
+        fig, ax = plt.subplots()
+        ax.pie(value_counts, autopct="%0.2f%%", labels=["Male", "Female"])
+        st.pyplot(fig)
+
+    with col2:
+        # draw bar plot
+        st.subheader('Bar chart')
+        fig, ax = plt.subplots()
+        ax.bar(value_counts.index, value_counts)
+        st.pyplot(fig)
+
+    with st.expander('Click here to display value counts'):
+        st.dataframe(value_counts)
